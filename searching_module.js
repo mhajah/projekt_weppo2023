c92e5a2e94d545477b3a4b16f9246f27ffbbe122
Michał Hajahmadov <m.hajahmadov@gmail.com>
2023-01-13 23:26:44 +0100

    update

--- searching_module.js
+++ searching_module.js
@@ -7,33 +7,30 @@ var app = express();
 app.post('/', (req, res,next) => {
     const search_value = req.body.name;
     //make question to the SQL or sth...app
-    // var sql = require("mssql")
-    // var config = {
-    //     server: 'localhost', //palceholder
-    //     datebase: 'Goods'
-    // };
+    var sql = require("mssql")
+    var config = {
+        server: 'localhost', //palceholder
+        datebase: 'Sklep'
+    };
 
     //conencting to the datebase
-    // sql.connect(config,function(err){
+    sql.connect(config,function(err){
 
     
-    //     if (err) console.log(err);
+        if (err) console.log(err);
         
-    //     //create Request object
-    //     var request = new sql.Request();
+        //create Request object
+        var request = new sql.Request();
 
-    //     //querry to the datebase and get the records
-    //     request.query('SELECT * FROM `table` WHERE `column` LIKE \'%{$search_value}%\'',function (err, recordset){
-    //         if(err) console.log(err)
+        //querry to the datebase and get the records
+        request.query('SELECT * FROM [Sklep].[dbo].[goods] WHERE name LIKE \'%{$search_value}%\'',function (err, recordset){
+            if(err) console.log(err)
 
-    //         //sending recors as a response
-    //         res.send(recordset);
-    //     });
-    // });
+            //sending recors as a response
+            res.send(recordset);
+        });
+    });
 
-const {Pool, Client} = require('pg')
 
-const pool = new Pool()
-const res = await pool.query('SELECT * FROM goods WHERE name  LIKE \'{$}\'')
 
 });
